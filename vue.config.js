3d6d82bfd697572c1a2e8868c659a728d0e4cd83
valery-chumak <blossssom88@gmail.com>
2023-10-10 17:13:07 +0300

    fix bugs

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath: "/vue-project/",
   transpileDependencies: true,
 });
+module.exports = {
+  publicPath: "/vue-project/",
+};
